b98517b72db6fec02adbfe79ec8a3fd4a810ddcc
Rocked03 <rockedohthree@gmail.com>
2026-04-04 18:06:38 +1300

    fix: database url

--- packages/backend/prisma.config.ts
+++ packages/backend/prisma.config.ts
@@ -1,9 +1,13 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+
+const DATABASE_URL =
+  process.env.DATABASE_URL ??
+  "postgresql://postgres:postgres@localhost:5432/postgres?schema=public";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: DATABASE_URL,
   },
 });
