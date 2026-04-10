4305b6a9d48f13e32deba7b336a217045e3e7d1d
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-04-11 10:49:14 +1200

    tweaks, timing functions

--- packages/frontend/src/components/button/Button.tsx
+++ packages/frontend/src/components/button/Button.tsx
@@ -2,6 +2,10 @@ import { Button as MuiButton, styled } from "@mui/material";
 
 export const Button = styled(MuiButton)`
   border: oklch(from var(--discord-white) l c h / 12%) 3px solid;
-  transition: var(--transition-duration-fast) ease;
-  transition-property: background-color, border-color, color;
+  transition-duration: var(--transition-duration-fast);
+  transition-property: background-color, border-color, color, scale;
+  transition-timing-function: ease, ease, ease, ease, var(--ease-out-quad);
+  &:active {
+    scale: 99%;
+  }
 `;
