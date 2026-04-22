9725b7aac84ebf3674e6eb79355d671cbbb44586
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-04-22 14:24:11 +1200

    fix CLS

--- packages/frontend/src/app/leaderboard/Leaderboard.tsx
+++ packages/frontend/src/app/leaderboard/Leaderboard.tsx
@@ -4,8 +4,7 @@ import { useCanvasContext } from "@/contexts";
 import { useLeaderboard } from "@/hooks/queries/useLeaderboard";
 import { styled } from "@mui/material";
 import Pagination from "@mui/material/Pagination";
-import PaginationItem from "@mui/material/PaginationItem";
-import { useEffect, useState } from "react";
+import { useId, useState } from "react";
 import LeaderboardRow, { LeaderboardRowSkeleton } from "./LeaderboardRow";
 
 const Wrapper = styled("div")`
@@ -40,22 +39,21 @@ const NoContentsMessage = styled("p")`
 
 export default function Leaderboard() {
   const { canvas } = useCanvasContext();
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(1); // TODO: Use URL for this state
+
   const {
-    data: { total, page: currentPage, size, entries: leaderboard } = {
-      total: undefined,
-      page: page,
-      size: 10,
-      entries: [],
-    },
-    isLoading: isLeaderboardLoading,
+    data: { total, page: currentPage, size = 10, entries } = {},
+    isFetching: isLeaderboardFetching,
   } = useLeaderboard(canvas.id, page);
+  const isLeaderboardEmpty = entries?.length === 0;
 
-  useEffect(() => {
-    if (canvas.id) setPage(1);
-  }, [canvas.id]);
+  const [prevCanvasId, setPrevCanvasId] = useState(canvas.id);
+  if (prevCanvasId !== canvas.id) {
+    setPage(1);
+    setPrevCanvasId(canvas.id);
+  }
 
-  const isLeaderboardEmpty = leaderboard.length === 0;
+  const listId = useId();
 
   return (
     <Wrapper>
@@ -65,42 +63,32 @@ export default function Leaderboard() {
       </TitleBlock>
 
       {/** biome-ignore lint/a11y/noRedundantRoles: <explanation> */}
-      <List start={leaderboard?.[0]?.rank} role="list">
-        {isLeaderboardLoading ?
+      <List
+        aria-busy={isLeaderboardFetching}
+        id={listId}
+        role="list"
+        start={entries?.[0]?.rank}
+      >
+        {isLeaderboardFetching ?
           Array.from({ length: 10 }, (_, index) => (
             <LeaderboardRowSkeleton key={index.toString()} />
           ))
         : isLeaderboardEmpty ?
           <NoContentsMessage>No leaderboard found</NoContentsMessage>
-        : leaderboard.map((entry) => (
+        : entries?.map((entry) => (
             <LeaderboardRow key={entry.userId} entry={entry} />
           ))
         }
       </List>
       <Pagination
+        aria-controls={listId}
+        color="primary"
+        count={total ? Math.ceil(total / size) : currentPage}
+        onChange={(_, value) => setPage(value)}
         page={page}
-        siblingCount={0}
-        boundaryCount={0}
+        shape="rounded"
         showFirstButton
         showLastButton
-        onChange={(_, value) => setPage(value)}
-        count={total ? Math.ceil(total / size) : currentPage}
-        color="primary"
-        size="large"
-        renderItem={(item) => {
-          switch (item.type) {
-            case "start-ellipsis":
-            case "end-ellipsis":
-              return null;
-            case "page":
-              if (item.page !== currentPage) return null;
-          }
-
-          return <PaginationItem {...item} />;
-        }}
-        sx={{
-          display: total === 0 ? "none" : "inherit",
-        }}
       />
     </Wrapper>
   );
