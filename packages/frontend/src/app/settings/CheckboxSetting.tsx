c77da51332935dbc2595608cec8ac35359634bb2
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-04-22 13:06:37 +1200

    remove unused imports

--- packages/frontend/src/app/settings/CheckboxSetting.tsx
+++ packages/frontend/src/app/settings/CheckboxSetting.tsx
@@ -1,6 +1,5 @@
 import { styled } from "@mui/material";
 import { useId } from "react";
-import useLocalStorage, { LocalStorageKey } from "./useLocalStorage";
 
 const Wrapper = styled("div")`
   align-items: baseline;
