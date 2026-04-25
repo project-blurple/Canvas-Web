e00e8390b17d3d578093b719e67423a5b43f0243
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-04-25 21:47:43 +1200

    chore(frontend): remove dead `useScreenDimensions` hook (#461)

--- packages/frontend/src/hooks/index.ts
+++ packages/frontend/src/hooks/index.ts
@@ -2,4 +2,3 @@ export * from "./queries";
 
 export { useCanvasImage } from "./useCanvasImage";
 export { useCanvasSearchParams } from "./useCanvasSearchParams";
-export { useScreenDimensions } from "./useScreenDimensions";
