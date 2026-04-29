5896d3e9c7f63cd95172b72613857ae5e1acd92c
Rocked03 <me@rocked03.dev>
2026-04-29 20:22:50 +1200

    feat: sounds and settings (#391)
    * feat: cooldown audio notification
    
    * feat: audio settings
    
    * feat: pick color and place pixel sounds
    
    * spacing
    
    * balance labels
    
    * again
    
    * initial redesign
    
    * remove debug border
    
    * tweaks
    
    * icons
    
    * fix for narrow viewports
    
    * explainer
    
    * white headings
    
    * The Great Width Unification™
    
    * header link
    
    * less awkward header font size
    
    * fix: linting
    
    * refactor(frontend): replace audio context with `useLocalStorage` (#436)
    
    * death to context
    
    * remove debug timing
    
    * remove unused imports
    
    * initialise input as controlled
    
    * rename utility type
    
    * ditch `T` type suffix
    
    * remove dead setting
    
    * remove dead code
    
    * fix type
    
    * `usePlaySound()`
    
    precedence
    
    fix
    
    * dependent setting
    
    * Revert "dependent setting"
    
    This reverts commit a14e7f6ee09fefc582a2c7df7f5fbf9011920a48.
    
    Honestly just wanted this in the Git history. I anticipate this funcionality being useful in future, but this particular application of it is incorrect.
    
    * refactor: small renames
    
    * fix: errors
    
    ---------
    
    Co-authored-by: S-Ou <me@sjou.dev>
    Co-authored-by: Jasper Lai <33956381+jaskfla@users.noreply.github.com>

---
+++ packages/frontend/src/app/settings/Settings.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { styled } from "@mui/material";
+import { BellRing } from "lucide-react";
+import CheckboxSetting from "./CheckboxSetting";
+import useLocalStorage from "./useLocalStorage";
+
+const Wrapper = styled("div")`
+  display: flex;
+  flex-direction: column;
+  margin-inline: auto;
+  padding-block: 4rem;
+  padding-inline: var(--layout-padding-x);
+  place-items: center;
+  > * {
+    inline-size: min(36rem, 100%);
+  }
+`;
+
+const H1 = styled("h1")`
+  text-align: center;
+`;
+
+const H2 = styled("h2")`
+  color: unset;
+  font-size: 1.125rem;
+  font-weight: 500;
+  margin-block-end: 1em;
+  margin-block-start: 5em;
+  .lucide {
+    block-size: 1em;
+    color: var(--discord-white);
+    display: inline-block;
+    inline-size: auto;
+    margin-inline-end: 0.5em;
+    opacity: 55%;
+    vertical-align: middle;
+  }
+`;
+
+const Form = styled("form")`
+  border-radius: var(--card-border-radius);
+  border: var(--card-border);
+`;
+
+const Aside = styled("aside")`
+  margin-block-start: 5rem;
+  opacity: 55%;
+  text-align: center;
+  text-wrap: balance;
+`;
+
+function SoundEffectsSetting() {
+  const [value, setValue] = useLocalStorage("audio/sound-fx");
+  return (
+    <CheckboxSetting
+      aria-busy={value === undefined}
+      checked={value}
+      description="Play sound effects as you interact with a canvas"
+      label="Sound effects"
+      name="sound-fx"
+      onChange={(e) => setValue(e.target.checked)}
+    />
+  );
+}
+
+function CooldownExpiryJingleSetting() {
+  const [value, setValue] = useLocalStorage("audio/cooldown-jingle");
+  return (
+    <CheckboxSetting
+      aria-busy={value === undefined}
+      checked={value}
+      description="Play a sound when you can place another pixel"
+      label="Cooldown expiry jingle"
+      name="cooldown-jingle"
+      onChange={(e) => setValue(e.target.checked)}
+    />
+  );
+}
+
+export default function Settings() {
+  return (
+    <Wrapper>
+      <H1>Settings</H1>
+
+      <H2>
+        <BellRing />
+        Notification sounds
+      </H2>
+      <Form>
+        <SoundEffectsSetting />
+        <CooldownExpiryJingleSetting />
+      </Form>
+
+      <Aside>
+        Settings are stored locally in your browser, and don&rsquo;t follow your
+        account
+      </Aside>
+    </Wrapper>
+  );
+}
