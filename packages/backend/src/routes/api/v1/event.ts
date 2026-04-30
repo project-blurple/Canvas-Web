871d013e9131d2f69ac9e9a80d02a909222a90f7
S-Ou <me@sjou.dev>
2026-05-01 00:25:57 +1200

    chore: unused import

--- packages/backend/src/routes/api/v1/event.ts
+++ packages/backend/src/routes/api/v1/event.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { ApiError, NotFoundError } from "@/errors";
+import { ApiError } from "@/errors";
 import {
   CreateEventBodyModel,
   EditEventBodyModel,
