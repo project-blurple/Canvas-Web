ac65a3cccfec8a183fa50a7cf324bbacb484ee26
S-Ou <me@sjou.dev>
2026-05-07 23:30:32 +1200

    feat: added discord user profile to blocklist

--- packages/backend/src/services/blocklistService.ts
+++ packages/backend/src/services/blocklistService.ts
@@ -1,13 +1,26 @@
 import type { BlocklistEntry } from "@blurple-canvas-web/types";
 import { prisma } from "@/client";
 
-export async function getBlocklist() {
-  return await prisma.blacklist.findMany({
+export async function getBlocklist(): Promise<BlocklistEntry[]> {
+  const blocklist = await prisma.blacklist.findMany({
     select: {
       user_id: true,
       date_added: true,
+      discord_user_profile: {
+        select: {
+          username: true,
+          profile_picture_url: true,
+        },
+      },
     },
   });
+
+  return blocklist.map((entry) => ({
+    userId: entry.user_id,
+    dateAdded: entry.date_added,
+    username: entry.discord_user_profile?.username ?? null,
+    profilePictureUrl: entry.discord_user_profile?.profile_picture_url ?? null,
+  }));
 }
 
 export async function userIsBlocklisted(
