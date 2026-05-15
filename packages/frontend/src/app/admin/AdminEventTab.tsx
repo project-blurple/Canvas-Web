1e0fb4f601728a921068ef970ad5a56ef8a86cf3
S-Ou <me@sjou.dev>
2026-05-15 18:42:16 +1200

    feat: selected event

--- packages/frontend/src/app/admin/AdminEventTab.tsx
+++ packages/frontend/src/app/admin/AdminEventTab.tsx
@@ -2,6 +2,7 @@
 
 import { styled } from "@mui/material";
 import { TabPanel } from "@/components/action-panel/tabs/ActionPanelTabBody";
+import { useCanvasContext } from "@/contexts/CanvasContext";
 import { useCanvasList, useEventInfo } from "@/hooks";
 
 const AdminEventTabBlock = styled(TabPanel)`
@@ -24,15 +25,16 @@ export default function AdminEventTab({
   active,
   ...props
 }: AdminEventTabProps) {
-  const { data: currentEvent, isLoading: currentEventIsLoading } =
-    useEventInfo();
+  const { canvas } = useCanvasContext();
+  const { data: selectedEvent, isLoading: currentEventIsLoading } =
+    useEventInfo(canvas.eventId ?? undefined);
   const { data: canvases = [], isLoading: canvasListIsLoading } =
     useCanvasList();
 
   const isLoading = currentEventIsLoading || canvasListIsLoading;
 
   const eventCanvases = canvases.filter(
-    (canvas) => canvas.eventId === currentEvent?.id,
+    (canvas) => canvas.eventId === selectedEvent?.id,
   );
 
   return (
@@ -40,10 +42,11 @@ export default function AdminEventTab({
       <EventInfoWrapper>
         {isLoading ?
           "Loading..."
-        : !currentEvent ?
+        : !selectedEvent ?
           <h2>Event not found</h2>
         : <>
-            <h2>{currentEvent.name}</h2>
+            <h2>{selectedEvent.name}</h2>
+            {selectedEvent.isCurrentEvent && <p>This is the current event.</p>}
             <ul>
               {/* This would be cool as previews/thumbnails */}
               {eventCanvases.map((canvas) => (
