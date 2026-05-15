6cdfb5272184d4493762cda67e1ceddbc8592646
S-Ou <me@sjou.dev>
2026-05-16 01:29:33 +1200

    style: some event page styling

--- packages/frontend/src/app/admin/AdminEventTab.tsx
+++ packages/frontend/src/app/admin/AdminEventTab.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { styled } from "@mui/material";
+import { CalendarRange, Radio } from "lucide-react";
 import { TabPanel } from "@/components/action-panel/tabs/ActionPanelTabBody";
 import { useCanvasContext } from "@/contexts/CanvasContext";
 import { useCanvasList, useEventInfo } from "@/hooks";
@@ -13,6 +14,23 @@ const EventInfoWrapper = styled("div")`
   display: flex;
   flex-direction: column;
   gap: 1rem;
+  width: 100%;
+`;
+
+const EventInfoHeader = styled("h1")`
+  align-items: center;
+  display: flex;
+  font-size: 1.5rem;
+  font-weight: 600;
+  gap: 0.5rem;
+`;
+
+const EventLiveIndicator = styled("div")`
+  align-items: center;
+  display: flex;
+  font-size: 0.875rem;
+  gap: 0.25rem;
+  opacity: 0.75;
 `;
 
 interface AdminEventTabProps extends React.ComponentPropsWithRef<
@@ -45,8 +63,16 @@ export default function AdminEventTab({
         : !selectedEvent ?
           <h2>Event not found</h2>
         : <>
-            <h2>{selectedEvent.name}</h2>
-            {selectedEvent.isCurrentEvent && <p>This is the current event.</p>}
+            <EventInfoHeader>
+              <CalendarRange />
+              {selectedEvent.name}
+            </EventInfoHeader>
+            {selectedEvent.isCurrentEvent && (
+              <EventLiveIndicator>
+                <Radio size={16} />
+                This event is currently live!
+              </EventLiveIndicator>
+            )}
             <ul>
               {/* This would be cool as previews/thumbnails */}
               {eventCanvases.map((canvas) => (
