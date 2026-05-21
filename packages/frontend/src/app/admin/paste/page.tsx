dbdbf58ca9859102e176768ea6376860a1520eea
S-Ou <me@sjou.dev>
2026-05-21 23:51:19 +1200

    feat: paste tab outline

--- packages/frontend/src/app/admin/paste/page.tsx
+++ packages/frontend/src/app/admin/paste/page.tsx
@@ -1,26 +1,56 @@
 "use client";
 
 import { styled } from "@mui/material";
+import { CanvasWrapper } from "@/app/Main";
+import ActionPanelPrimitives from "@/components/action-panel/primitives";
+import {
+  ActionPanelTabBody,
+  FullWidthScrollView,
+} from "@/components/action-panel/tabs/ActionPanelTabBody";
+import { CanvasView } from "@/components/canvas";
+import { SlideableDrawer } from "@/components/slideable-drawer";
 import AdminDashboard from "../AdminDashboard";
 
 const AdminPasteTabBlock = styled("section")`
   display: block;
-  max-width: 80rem;
   width: 100%;
 `;
 
-const PasteWrapper = styled("div")`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  width: 100%;
+const PasteWrapper = styled(CanvasWrapper)`
+  body:has(&) {
+    --action-panel-width: 40rem;
+
+    ${({ theme }) => theme.breakpoints.up("lg")} {
+      --action-panel-width: 50rem;
+    }
+  }
 `;
+
+function AdminDashboardPasteActionPanel() {
+  return (
+    <ActionPanelPrimitives.Root>
+      <FullWidthScrollView>
+        <ActionPanelTabBody>hi</ActionPanelTabBody>
+      </FullWidthScrollView>
+    </ActionPanelPrimitives.Root>
+  );
+}
 
 function AdminPasteTab() {
   return (
     <AdminPasteTabBlock>
-      <PasteWrapper>WIP</PasteWrapper>
+      <PasteWrapper>
+        <CanvasView
+          actionPanel={<AdminDashboardPasteActionPanel />}
+          canvasLabel="Admin paste"
+          showInvite={false}
+          showNotices={false}
+          showReticle={false}
+        />
+        <SlideableDrawer>
+          <AdminDashboardPasteActionPanel />
+        </SlideableDrawer>
+      </PasteWrapper>
     </AdminPasteTabBlock>
   );
 }
