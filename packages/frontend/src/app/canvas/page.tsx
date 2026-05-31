d40ffb4cf257678f696fbb7d50e85d3ea30db927
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-06-01 10:52:06 +1200

    redirect `/canvas` to current canvas

---
+++ packages/frontend/src/app/canvas/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+import { fetchCanvasInfo } from "@/hooks/queries/serverFetch";
+
+export default async function Page() {
+  const canvasInfo = await fetchCanvasInfo();
+  redirect(`/canvas/${canvasInfo.id}`);
+}
