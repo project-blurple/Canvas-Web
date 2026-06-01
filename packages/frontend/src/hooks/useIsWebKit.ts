32f1b0460025e1f5d54b9c86926a7a53dcf6b6cf
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-06-01 15:12:04 +1200

    comment

--- packages/frontend/src/hooks/useIsWebKit.ts
+++ packages/frontend/src/hooks/useIsWebKit.ts
@@ -2,12 +2,6 @@
 
 import { useMemo } from "react";
 
-/**
- * Detects WebKit browsers (Safari) to disable transition animations that cause
- * blurry canvas rendering.
- * @see https://bugs.webkit.org/show_bug.cgi?id=27684
- * If the user spoofs their user agent, this is not my problem.
- */
 export default function useIsWebKit(): boolean {
   return useMemo(() => {
     const { userAgent: ua, vendor } = navigator;
