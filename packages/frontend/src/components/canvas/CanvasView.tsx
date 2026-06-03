13753877170e31924ad8ce60b02107958e16f97b
S-Ou <me@sjou.dev>
2026-06-04 00:34:42 +1200

    style: even more subtle shadow

--- packages/frontend/src/components/canvas/CanvasView.tsx
+++ packages/frontend/src/components/canvas/CanvasView.tsx
@@ -244,7 +244,8 @@ const CanvasImageWrapper = styled("div", {
       filter: grayscale(80%);
     `}
 
-  box-shadow: 0 0 2rem 0 rgba(0 0 0 / 0.5); // No shadow behind the transparent pixels
+  box-shadow: 0 0 100px 0 rgba(0 0 0 / 0.15);
+  // box-shadow means the shadow only shows on the edges, and doesn't affect the backing color of the translucent pixels in the canvas
   position: relative;
 
   img {
