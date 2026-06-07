789a3780569aa94be1009241554a5845740e3cda
S-Ou <me@sjou.dev>
2026-06-07 21:14:43 +1200

    feat: history span tags

--- packages/backend/src/utils/otel.ts
+++ packages/backend/src/utils/otel.ts
@@ -8,12 +8,23 @@ export type RequestWithRootSpan = {
 
 export function addSpanAttributes(
   req: RequestWithRootSpan,
-  attributes: Record<string, string | number | boolean | null | undefined>,
+  attributes: Record<
+    string,
+    | string
+    | number
+    | boolean
+    | null
+    | undefined
+    | Array<null | undefined | string>
+  >,
 ): void {
   const activeSpan = trace.getActiveSpan();
   const rootSpan = req.__otelRootSpan;
 
-  const filteredAttributes: Record<string, string | number | boolean> = {};
+  const filteredAttributes: Record<
+    string,
+    string | number | boolean | Array<null | undefined | string>
+  > = {};
   for (const [key, value] of Object.entries(attributes)) {
     if (value !== null && value !== undefined) {
       filteredAttributes[key] = value;
