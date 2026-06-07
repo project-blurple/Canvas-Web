d48e89935188259332f41a39e02d0066f6dbab9a
S-Ou <me@sjou.dev>
2026-06-07 17:41:10 +1200

    feat: base otel traces

---
+++ packages/backend/src/tracing.ts
@@ -0,0 +1,37 @@
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import { resourceFromAttributes } from "@opentelemetry/resources";
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
+import config from "./config";
+
+const exporter = new OTLPTraceExporter({
+  url: config.tracing.otlpTracesEndpoint,
+});
+
+const sdk = new NodeSDK({
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: config.tracing.serviceName,
+  }),
+  traceExporter: exporter,
+});
+
+registerInstrumentations({
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+try {
+  sdk.start();
+  console.info("OpenTelemetry initialized");
+} catch (err: unknown) {
+  console.error("Error starting OpenTelemetry", err);
+}
+
+process.on("SIGTERM", () => {
+  sdk
+    .shutdown()
+    .catch((err: unknown) =>
+      console.error("Error shutting down OpenTelemetry", err),
+    );
+});
