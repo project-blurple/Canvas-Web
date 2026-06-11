e596c51020e1f4cdcccfeb131317cb44a326eaea
Stijn van der Kolk <stijn@kolkies.dev>
2026-06-11 10:26:10 +0300

    feat(backend/nest): port discord token refresh service

--- packages/backend-nest/src/discord/discord.module.ts
+++ packages/backend-nest/src/discord/discord.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 
 import { DiscordProfileService } from "@/discord/discord-profile.service";
+import { DiscordTokenService } from "@/discord/discord-token.service";
 
 @Module({
-  providers: [DiscordProfileService],
-  exports: [DiscordProfileService],
+  providers: [DiscordProfileService, DiscordTokenService],
+  exports: [DiscordProfileService, DiscordTokenService],
 })
 export class DiscordModule {}
