620963dd0ce08d8038e3d19c780e590efd997a19
Stijn van der Kolk <stijn@kolkies.dev>
2026-06-11 19:54:33 +0300

    feat(swagger): add enhanced Swagger setup

--- packages/backend-nest/src/main.ts
+++ packages/backend-nest/src/main.ts
@@ -1,23 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import type { NestExpressApplication } from "@nestjs/platform-express";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "@/app.module";
 import { configureApp } from "@/app.setup";
 import { type AppConfig, appConfig } from "@/config/app.config";
+import { setupSwagger } from "@/swagger.setup";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   configureApp(app);
-
-  const config = new DocumentBuilder()
-    .setTitle("Blurple Canvas API")
-    .setDescription("API for the Blurple Canvas project")
-    // current day
-    .setVersion(new Date().toISOString().split("T")[0])
-    .build();
-
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("docs", app, documentFactory);
+  setupSwagger(app);
 
   const { port } = app.get<AppConfig>(appConfig.KEY);
   app.enableShutdownHooks();
