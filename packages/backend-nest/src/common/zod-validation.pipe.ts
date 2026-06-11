6e017a2ca774c58c83ec3714f73d87f76bd90141
Stijn van der Kolk <stijn@kolkies.dev>
2026-06-11 23:37:10 +0300

    fix(backend/nest): security fix and upgrade to ts 6 + es2025

--- packages/backend-nest/src/common/zod-validation.pipe.ts
+++ packages/backend-nest/src/common/zod-validation.pipe.ts
@@ -2,11 +2,12 @@ import { createZodValidationPipe } from "nestjs-zod";
 import { ZodError } from "zod";
 import { BadRequestError } from "./errors/bad-request.error";
 
-export const ZodValidationPipe = createZodValidationPipe({
-  createValidationException: (error) =>
-    new BadRequestError(
-      "Invalid request data",
-      error instanceof ZodError ? error.issues : [],
-    ),
-  strictSchemaDeclaration: true,
-});
+export const ZodValidationPipe: ReturnType<typeof createZodValidationPipe> =
+  createZodValidationPipe({
+    createValidationException: (error) =>
+      new BadRequestError(
+        "Invalid request data",
+        error instanceof ZodError ? error.issues : [],
+      ),
+    strictSchemaDeclaration: true,
+  });
