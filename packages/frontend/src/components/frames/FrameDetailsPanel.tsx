15117bfea5020ab0bc36ab1696235f3fe45cf38a
S-Ou <me@sjou.dev>
2026-06-19 00:53:05 +1200

    feat: control buttons

--- packages/frontend/src/components/frames/FrameDetailsPanel.tsx
+++ packages/frontend/src/components/frames/FrameDetailsPanel.tsx
@@ -1,14 +1,28 @@
+import { styled } from "@mui/material";
+import { ArrowLeftFromLine, Crosshair, SquarePen } from "lucide-react";
 import { useSelectedFrameContext } from "@/contexts/SelectedFrameContext";
 import ActionPanelPrimitives from "../action-panel/primitives";
 import { ActionPanelTabBody } from "../action-panel/tabs/ActionPanelTabBody";
 import { FramePanelMode } from "../action-panel/tabs/FramesTab";
+import { BasicButton } from "../button";
+
+const ControlButtonRow = styled("div")`
+  background: transparent;
+  display: flex;
+  gap: 0.5rem;
+  padding: 0;
+
+  > * {
+    flex: 1;
+  }
+`;
 
 export default function FrameDetailsPanel({
   setActivePanel,
 }: {
   setActivePanel: (panel: FramePanelMode) => void;
 }) {
-  const { frame } = useSelectedFrameContext();
+  const { frame, setFrame } = useSelectedFrameContext();
 
   if (!frame) {
     setActivePanel(FramePanelMode.List);
@@ -17,6 +31,17 @@ export default function FrameDetailsPanel({
   return (
     <div>
       <ActionPanelTabBody>
+        <ControlButtonRow>
+          <BasicButton onClick={() => setFrame(null)}>
+            <ArrowLeftFromLine />
+          </BasicButton>
+          <BasicButton>
+            <Crosshair />
+          </BasicButton>
+          <BasicButton>
+            <SquarePen />
+          </BasicButton>
+        </ControlButtonRow>
         <div>
           <ActionPanelPrimitives.SectionHeading>
             Frame details
