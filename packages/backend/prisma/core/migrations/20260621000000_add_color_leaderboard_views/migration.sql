24dc9b5440c8d925d48cc0c4ea46515f9c2b2952
S-Ou <me@sjou.dev>
2026-06-21 02:50:23 +1200

    feat: color leaderboards

---
+++ packages/backend/prisma/core/migrations/20260621000000_add_color_leaderboard_views/migration.sql
@@ -0,0 +1,66 @@
+DROP VIEW IF EXISTS color_leaderboard;
+
+DROP VIEW IF EXISTS color_leaderboard_frame;
+
+CREATE VIEW color_leaderboard AS
+SELECT
+  history.user_id,
+  history.canvas_id,
+  history.color_id,
+  count(*) AS total_pixels,
+  rank() OVER (
+    PARTITION BY
+      history.canvas_id,
+      history.color_id
+    ORDER BY
+      (count(*)) DESC
+  ) AS rank
+FROM
+  history
+WHERE
+  history.user_id NOT IN (
+    SELECT
+      user_id
+    FROM
+      blacklist
+  )
+GROUP BY
+  history.user_id,
+  history.canvas_id,
+  history.color_id;
+
+CREATE VIEW color_leaderboard_frame AS
+SELECT
+  history.user_id,
+  history.canvas_id,
+  frame.id as frame_id,
+  history.color_id,
+  count(*) AS total_pixels,
+  rank() OVER (
+    PARTITION BY
+      history.canvas_id,
+      frame.id,
+      history.color_id
+    ORDER BY
+      (count(*)) DESC
+  ) AS rank
+FROM
+  history
+INNER JOIN frame ON
+  history.canvas_id = frame.canvas_id
+  AND history.x >= frame.x_0
+  AND history.x <= frame.x_1
+  AND history.y >= frame.y_0
+  AND history.y <= frame.y_1
+WHERE
+  history.user_id NOT IN (
+    SELECT
+      user_id
+    FROM
+      blacklist
+  )
+GROUP BY
+  history.user_id,
+  history.canvas_id,
+  frame.id,
+  history.color_id;
