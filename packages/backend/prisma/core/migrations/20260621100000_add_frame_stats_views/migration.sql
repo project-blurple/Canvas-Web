2d4b49d477a171da65fdbcedd74aef2c7ac3371b
S-Ou <me@sjou.dev>
2026-06-21 15:37:04 +1200

    feat: color distribution views

--- packages/backend/prisma/core/migrations/20260621100000_add_frame_stats_views/migration.sql
+++ packages/backend/prisma/core/migrations/20260621100000_add_frame_stats_views/migration.sql
@@ -23,3 +23,54 @@ WHERE
 GROUP BY
   frame.id,
   frame.canvas_id;
+
+CREATE VIEW canvas_colors AS
+SELECT
+  history.canvas_id,
+  history.color_id,
+  count(*) AS total_pixels
+FROM
+  history
+WHERE
+  history.user_id NOT IN (
+    SELECT
+      user_id
+    FROM
+      blacklist
+  )
+GROUP BY
+  history.canvas_id,
+  history.color_id
+ORDER BY
+  history.canvas_id,
+  total_pixels DESC
+
+CREATE VIEW frame_colors AS
+SELECT
+  frame.id AS frame_id,
+  history.canvas_id,
+  history.color_id,
+  count(*) AS total_pixels
+FROM
+  history
+INNER JOIN frame ON
+  history.canvas_id = frame.canvas_id
+  AND history.x >= frame.x_0
+  AND history.x <= frame.x_1
+  AND history.y >= frame.y_0
+  AND history.y <= frame.y_1
+WHERE
+  history.user_id NOT IN (
+    SELECT
+      user_id
+    FROM
+      blacklist
+  )
+GROUP BY
+  frame.id,
+  history.canvas_id,
+  history.color_id
+ORDER BY
+  history.canvas_id,
+  frame.id,
+  total_pixels DESC;
